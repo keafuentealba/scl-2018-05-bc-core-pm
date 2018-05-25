d7200b341f3c10d466cb2bf8fee57ca28bb0b430
camila fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-05-25 18:29:59 -0400

    css

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 window.actions = {
   execute: () => {
-    let offsetEntry = document.getElementById("offsetNumber");
+    let offsetEntry = document.getElementById("offsetNumber").value;
     console.log(offsetEntry)
+    let option1 = document.getElementById("translatorOptions1").checked;
+    console.log(option1)
+    let option2 = document.getElementById("translatorOptions2").checked;
+    console.log(option2)
   }
 }
