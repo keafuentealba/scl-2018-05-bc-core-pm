0141eee48325ec7d5bf67350e282da02f4fea57a
camila fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-05-27 23:31:20 -0400

    diagramacioncss alert de options

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 window.actions = {
   execute: () => {
     let offsetEntry = document.getElementById("offsetNumber").value;
-    console.log(offsetEntry)
     let option1 = document.getElementById("translatorOptions1").checked;
-    console.log(option1)
     let option2 = document.getElementById("translatorOptions2").checked;
-    console.log(option2)
+    let textReturn = document.getElementById('translator').value;
+    if(option1 == true){
+      alert('cifrado');
+    }
+  },
+  clean: () => {
+    alert('limpia');
+  
   }
+
 }
