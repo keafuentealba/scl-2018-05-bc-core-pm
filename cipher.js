9830aabc9bededd20b54445a892382ea4d2a975c
camila fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-06-01 09:27:32 -0400

    cipher decode

--- cipher.js
+++ cipher.js
@@ -11,8 +11,14 @@ window.cipher = {
   },
 
   decode: (textReturn, offsetEntry) => {
-    alert(textReturn);
-    alert(offsetEntry);
+    let i;
+    let resultado = '';
+    for (i = 0; i < textReturn.length; i++){
+      let x = textReturn.toUpperCase().charCodeAt(i);
+      let resultadoFormula = (x - 65 - offsetEntry) % 26 + 65;
+      resultado += String.fromCharCode(resultadoFormula);
 
+    }
+    return resultado;
   }
 }
